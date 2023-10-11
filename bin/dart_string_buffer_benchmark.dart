ed4e047a4709c8b0fd6968fac1acc75251193172
YumNumm <onory0917@gmail.com>
2023-10-12 03:42:07 +0900

    add: arguments feature

--- bin/dart_string_buffer_benchmark.dart
+++ bin/dart_string_buffer_benchmark.dart
@@ -1,9 +1,67 @@
-import 'dart:math';
+import 'dart:io';
 
-import 'package:dart_string_buffer_benchmark/dart_string_buffer_benchmark.dart'
-    as dart_string_buffer_benchmark;
+import 'package:args/args.dart';
+import 'package:dart_string_buffer_benchmark/dart_string_buffer_benchmark.dart';
 
-void main(List<String> arguments) async =>
-    dart_string_buffer_benchmark.StringBufferBenchmark().run(
-      max: pow(2, 15).toInt(),
+void main(List<String> arguments) async {
+  final parser = ArgParser()
+    ..addFlag('help', abbr: 'h', help: 'Show this help message and exit')
+    ..addOption(
+      'max',
+      abbr: 'm',
+      help: 'Max number of iterations',
+      defaultsTo: '1000',
+    )
+    ..addOption(
+      'step',
+      abbr: 's',
+      help: 'Step of iterations',
+      defaultsTo: '100',
+    )
+    ..addOption(
+      'repeat',
+      abbr: 'r',
+      help: 'Number of repeats for each iteration',
+      defaultsTo: '10',
+    )
+    ..addOption(
+      'output',
+      abbr: 'o',
+      help: 'Output csv file name',
+      defaultsTo: 'benchmark.csv',
+    )
+    ..addOption(
+      'type',
+      abbr: 't',
+      help: 'Benchmark type',
+      allowed: BenchmarkType.values.map(
+        (e) => e.name,
+      ),
+      defaultsTo: BenchmarkType.all.name,
+    )
+    ..addFlag(
+      'visualize',
+      abbr: 'v',
+      help: 'Visualize benchmark result. Output png file will be created.',
     );
+
+  final results = parser.parse(arguments);
+  final args = BenchmarkParameter.fromResults(results);
+  if (args.help) {
+    print(parser.usage);
+    exit(0);
+  }
+
+  await StringBufferBenchmark().run(
+    args,
+  );
+  if (args.visualize) {
+    // run python script
+    final process = await Process.start(
+      'python3',
+      [
+        'visualization/src/visualization/main.py',
+      ],
+    );
+  }
+}
--- lib/dart_string_buffer_benchmark.dart
+++ lib/dart_string_buffer_benchmark.dart
@@ -1,47 +1,65 @@
 import 'dart:io';
-import 'dart:math';
+
+import 'package:args/args.dart';
 
 class StringBufferBenchmark {
-  Future<void> run({int max = 1000}) async {
+  Future<void> run(BenchmarkParameter args) async {
+    // 1 stepで増やすiteration数
+    final stepCount = args.max ~/ args.step;
+
+    final types = switch (args.type) {
+      BenchmarkType.all => [...BenchmarkType.values]..remove(BenchmarkType.all),
+      _ => [args.type],
+    };
+
     final results = <BenchmarkResult>[];
-    final step = max ~/ 100;
-    for (var i = 1; i <= max; i += step) {
-      results
-        ..add(_runBenchmark(i, BenchmarkType.string))
-        ..add(_runBenchmark(i, BenchmarkType.stringBuffer));
+    for (var i = 1; i <= args.max; i += stepCount) {
+      for (final type in types) {
+        final result = _runBenchmark(i, type, args.repeat);
+        results.add(result);
+      }
     }
-    for (final result in results) {
-      print('${result.type}, ${result.loop}, ${result.time}ms');
-    }
-    // output as csv
+    await _outputAsCsv(args.output, results);
+  }
+
+  Future<void> _outputAsCsv(
+    String filename,
+    List<BenchmarkResult> results,
+  ) async {
     final csv = [
-      'type,x,y',
+      'type,loop,time',
       ...results.map((e) => '${e.type},${e.loop},${e.time}'),
     ].join('\n');
-    final file = File('benchmark.csv');
+    final file = File(filename);
     await file.writeAsString(csv);
   }
 
-  BenchmarkResult _runBenchmark(int loop, BenchmarkType type) {
+  BenchmarkResult _runBenchmark(int loop, BenchmarkType type, int repeat) {
     final stopwatch = Stopwatch()..start();
-    final random = Random();
-    switch (type) {
-      case BenchmarkType.string:
-        var buffer = '';
-        for (var i = 0; i < loop; i++) {
-          // ignore: use_string_buffers
-          buffer += random.nextInt(10).toString();
-        }
-        final _ = buffer;
-      case BenchmarkType.stringBuffer:
-        final buffer = StringBuffer();
-        for (var i = 0; i < loop; i++) {
-          buffer.write(random.nextInt(10).toString());
-        }
-        final _ = buffer.toString();
+    for (var i = 0; i < loop; i++) {
+      switch (type) {
+        case BenchmarkType.string:
+          // ignore: unused_local_variable
+          var buffer = '';
+          for (var i = 0; i < loop; i++) {
+            // ignore: use_string_buffers
+            buffer += 'a';
+          }
+        case BenchmarkType.stringBuffer:
+          final buffer = StringBuffer();
+          for (var i = 0; i < loop; i++) {
+            buffer.write('a');
+          }
+        case BenchmarkType.all:
+          throw UnimplementedError();
+      }
     }
     stopwatch.stop();
-    return BenchmarkResult(loop, stopwatch.elapsedMicroseconds / 1000, type);
+    return BenchmarkResult(
+      loop,
+      stopwatch.elapsedMicroseconds / 1000 / repeat,
+      type,
+    );
   }
 }
 
@@ -59,5 +77,46 @@ class BenchmarkResult {
 enum BenchmarkType {
   string,
   stringBuffer,
+  all,
   ;
+}
+
+class BenchmarkParameter {
+  BenchmarkParameter({
+    required this.help,
+    required this.max,
+    required this.step,
+    required this.repeat,
+    required this.output,
+    required this.type,
+    required this.visualize,
+  });
+
+  factory BenchmarkParameter.fromResults(ArgResults results) {
+    final help = results['help'] as bool;
+    final max = int.parse(results['max'] as String);
+    final step = int.parse(results['step'] as String);
+    final repeat = int.parse(results['repeat'] as String);
+    final output = results['output'] as String;
+    final type = results['type'] as String;
+    final visualize = results['visualize'] as bool;
+    return BenchmarkParameter(
+      help: help,
+      max: max,
+      step: step,
+      repeat: repeat,
+      output: output,
+      type: BenchmarkType.values.firstWhere(
+        (e) => e.name == type,
+      ),
+      visualize: visualize,
+    );
+  }
+  final bool help;
+  final int max;
+  final int step;
+  final int repeat;
+  final String output;
+  final BenchmarkType type;
+  final bool visualize;
 }
